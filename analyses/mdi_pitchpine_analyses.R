ad44b4b675f1293e7f43df20d247ce493b0d22f7
SmithEcophysLab <nick.gregory.smith@gmail.com>
2020-07-06 10:39:08 -0500

    some revisions based on Jeff's writing

--- analyses/mdi_pitchpine_analyses.R
+++ analyses/mdi_pitchpine_analyses.R
@@ -49,23 +49,39 @@ data_Name_means = summarise(data_group_by_Name,
                             Slope_mean = mean(Slope, na.rm = T),
                             Aspect_mean = mean(Aspect, na.rm = T))
 
+## create an elevation factor
+data$elevation_fac[data$Name == 'CAD' | data$Name == 'STSAUV'] = 'high'
+data$elevation_fac[data$Name == 'CADCLIFFS' | data$Name == 'WOND'] = 'low'
+
+## create a generic variable set to pass to formula argument
+ind_variables = c('elevation_fac', 'fire')
+dep_variables = c("log(Elevation)", "log(height)", "log(canopy)", "log(diam)",
+                  "d13C", "d15N", "C_foliar", "N_foliar", "CN_foliar", "Ca_foliar", "log(P_foliar)",
+                  "log(K_foliar)", "Mg_foliar", "Al_foliar", "log(Zn_foliar)", 
+                  "Ca_soil", "log(P_soil)", "K_soil", "Mg_soil", "log(Al_soil)", "log(Zn_soil)", 
+                  "pH", "CEC", "C_soil", "N_soil", "log(CN_soil)", "asin(sqrt(0.01 * retention))")
+
 ## fit models and explore results
 
 ### elevation
-Elevation_lm = lm(log(Elevation) ~ Name, data = data)
+Elevation_lm = lm(as.formula(paste(dep_variables[1],
+                                   paste(ind_variables, collapse = "*"),
+                                   sep = "~")), data = data)
 #plot(resid(Elevation_lm) ~ fitted(Elevation_lm))
 Anova(Elevation_lm)
-cld(emmeans(Elevation_lm, ~Name), alpha = 0.1)
+cld(emmeans(Elevation_lm, ~elevation_fac * fire))
 
 ### height
-height_lm = lm(log(height) ~ Name, data = data)
+height_lm = lm(as.formula(paste(dep_variables[2],
+                                paste(ind_variables, collapse = "*"),
+                                sep = "~")), data = data)
 #plot(resid(height_lm) ~ fitted(height_lm))
 Anova(height_lm)
-cld(emmeans(height_lm, ~Name), alpha = 0.1)
+cld(emmeans(height_lm, ~elevation_fac * fire))
 
-height_lmer_cont = lmer(log(height) ~ Elevation * fire + (1|Name), data = data)
-Anova(height_lmer_cont)
-test(emtrends(height_lmer_cont, ~fire, var = 'Elevation'))
+# height_lmer_cont = lmer(log(height) ~ Elevation * fire + (1|Name), data = data)
+# Anova(height_lmer_cont)
+# test(emtrends(height_lmer_cont, ~fire, var = 'Elevation'))
 
 height_plot = ggplot(data = data, aes(x = Name, y = log(height), col = fire)) +
   theme(legend.position = "none", 
@@ -97,14 +113,16 @@ multiplot(height_plot, height_plot_elevation, cols = 2)
 dev.off()
 
 ### canopy
-canopy_lm = lm(log(canopy) ~ Name, data = data)
+canopy_lm = lm(as.formula(paste(dep_variables[3],
+                                paste(ind_variables, collapse = "*"),
+                                sep = "~")), data = data)
 #plot(resid(canopy_lm) ~ fitted(canopy_lm))
 anova(canopy_lm)
-cld(emmeans(canopy_lm, ~Name), alpha = 0.1)
+cld(emmeans(canopy_lm, ~elevation_fac * fire))
 
-canopy_lmer_cont = lmer(log(canopy) ~ Elevation * fire + (1|Name), data = data)
-Anova(canopy_lmer_cont)
-test(emtrends(canopy_lmer_cont, ~fire, var = 'Elevation'))
+# canopy_lmer_cont = lmer(log(canopy) ~ Elevation * fire + (1|Name), data = data)
+# Anova(canopy_lmer_cont)
+# test(emtrends(canopy_lmer_cont, ~fire, var = 'Elevation'))
 
 canopy_plot = ggplot(data = data, aes(x = Name, y = log(canopy), col = fire)) +
   theme(legend.position = "none", 
@@ -136,14 +154,16 @@ multiplot(canopy_plot, canopy_plot_elevation, cols = 2)
 dev.off()
 
 ### diam
-diam_lm = lm(log(diam) ~ Name, data = data)
+diam_lm = lm(as.formula(paste(dep_variables[4],
+                              paste(ind_variables, collapse = "*"),
+                              sep = "~")), data = data)
 #plot(resid(diam_lm) ~ fitted(diam_lm))
 anova(diam_lm)
-cld(emmeans(diam_lm, ~Name), alpha = 0.1)
+cld(emmeans(diam_lm, ~elevation_fac * fire))
 
-diam_lmer_cont = lmer(log(diam) ~ Elevation * fire + (1|Name), data = data)
-Anova(diam_lmer_cont)
-test(emtrends(diam_lmer_cont, ~fire, var = 'Elevation'))
+# diam_lmer_cont = lmer(log(diam) ~ Elevation * fire + (1|Name), data = data)
+# Anova(diam_lmer_cont)
+# test(emtrends(diam_lmer_cont, ~fire, var = 'Elevation'))
 
 diam_plot = ggplot(data = data, aes(x = Name, y = log(diam), col = fire)) +
   theme(legend.position = "none", 
@@ -175,208 +195,254 @@ multiplot(diam_plot, diam_plot_elevation, cols = 2)
 dev.off()
 
 ### d13C
-d13C_lm = lm((d13C) ~ Name, data = data)
+d13C_lm = lm(as.formula(paste(dep_variables[5],
+                              paste(ind_variables, collapse = "*"),
+                              sep = "~")), data = data)
 #plot(resid(d13C_lm) ~ fitted(d13C_lm))
 anova(d13C_lm)
-cld(emmeans(d13C_lm, ~Name))
+cld(emmeans(d13C_lm, ~elevation_fac * fire))
 
 ggplot(data = data, aes(x = Name, y = (d13C))) +
   geom_boxplot()
 
 ### d15N
-d15N_lm = lm((d15N) ~ Name, data = data)
+d15N_lm = lm(as.formula(paste(dep_variables[6],
+                              paste(ind_variables, collapse = "*"),
+                              sep = "~")), data = data)
 #plot(resid(d15N_lm) ~ fitted(d15N_lm))
 anova(d15N_lm)
-cld(emmeans(d15N_lm, ~Name))
+cld(emmeans(d15N_lm, ~elevation_fac * fire))
 
 ggplot(data = data, aes(x = Name, y = (d15N))) +
   geom_boxplot()
 
 ### C_foliar
-C_foliar_lm = lm((C_foliar) ~ Name, data = data)
+C_foliar_lm = lm(as.formula(paste(dep_variables[7],
+                                  paste(ind_variables, collapse = "*"),
+                                  sep = "~")), data = data)
 #plot(resid(C_foliar_lm) ~ fitted(C_foliar_lm))
 anova(C_foliar_lm)
-cld(emmeans(C_foliar_lm, ~Name))
+cld(emmeans(C_foliar_lm, ~elevation_fac * fire))
 
 ggplot(data = data, aes(x = Name, y = (C_foliar))) +
   geom_boxplot()
 
 ### N_foliar
-N_foliar_lm = lm((N_foliar) ~ Name, data = subset(data, N_foliar < 5))
+N_foliar_lm = lm(as.formula(paste(dep_variables[8],
+                                  paste(ind_variables, collapse = "*"),
+                                  sep = "~")), data = data)
 #plot(resid(N_foliar_lm) ~ fitted(N_foliar_lm))
 anova(N_foliar_lm)
-cld(emmeans(N_foliar_lm, ~Name))
+cld(emmeans(N_foliar_lm, ~elevation_fac * fire))
 
 ggplot(data = subset(data, N_foliar < 5), aes(x = Name, y = (N_foliar))) +
   geom_boxplot()
 
 ### CN_foliar
-CN_foliar_lm = lm((CN_foliar) ~ Name, data = subset(data, N_foliar < 5))
+CN_foliar_lm = lm(as.formula(paste(dep_variables[9],
+                                   paste(ind_variables, collapse = "*"),
+                                   sep = "~")), data = data)
 #plot(resid(CN_foliar_lm) ~ fitted(CN_foliar_lm))
 anova(CN_foliar_lm)
-cld(emmeans(CN_foliar_lm, ~Name))
+cld(emmeans(CN_foliar_lm, ~elevation_fac * fire))
 
 ggplot(data = subset(data, N_foliar < 5), aes(x = Name, y = (CN_foliar))) +
   geom_boxplot()
 
 ### Ca_foliar
-Ca_foliar_lm = lm((Ca_foliar) ~ Name, data = data)
+Ca_foliar_lm = lm(as.formula(paste(dep_variables[10],
+                                   paste(ind_variables, collapse = "*"),
+                                   sep = "~")), data = data)
 #plot(resid(Ca_foliar_lm) ~ fitted(Ca_foliar_lm))
 anova(Ca_foliar_lm)
-cld(emmeans(Ca_foliar_lm, ~Name))
+cld(emmeans(Ca_foliar_lm, ~elevation_fac * fire))
 
 ggplot(data = data, aes(x = Name, y = (Ca_foliar))) +
   geom_boxplot()
 
 ### P_foliar
-P_foliar_lm = lm(log(P_foliar) ~ Name, data = data)
+P_foliar_lm = lm(as.formula(paste(dep_variables[11],
+                                  paste(ind_variables, collapse = "*"),
+                                  sep = "~")), data = data)
 #plot(resid(P_foliar_lm) ~ fitted(P_foliar_lm))
 anova(P_foliar_lm)
-cld(emmeans(P_foliar_lm, ~Name))
+cld(emmeans(P_foliar_lm, ~elevation_fac * fire))
 
 ggplot(data = data, aes(x = Name, y = (P_foliar))) +
   geom_boxplot()
 
 ### K_foliar
-K_foliar_lm = lm(log(K_foliar) ~ Name, data = data)
+K_foliar_lm = lm(as.formula(paste(dep_variables[12],
+                                  paste(ind_variables, collapse = "*"),
+                                  sep = "~")), data = data)
 #plot(resid(K_foliar_lm) ~ fitted(K_foliar_lm))
 anova(K_foliar_lm)
-cld(emmeans(K_foliar_lm, ~Name))
+cld(emmeans(K_foliar_lm, ~elevation_fac * fire))
 
 ggplot(data = data, aes(x = Name, y = (K_foliar))) +
   geom_boxplot()
 
 ### Mg_foliar
-Mg_foliar_lm = lm((Mg_foliar) ~ Name, data = data)
+Mg_foliar_lm = lm(as.formula(paste(dep_variables[13],
+                                   paste(ind_variables, collapse = "*"),
+                                   sep = "~")), data = data)
 #plot(resid(Mg_foliar_lm) ~ fitted(Mg_foliar_lm))
 anova(Mg_foliar_lm)
-cld(emmeans(Mg_foliar_lm, ~Name))
+cld(emmeans(Mg_foliar_lm, ~elevation_fac * fire))
 
 ggplot(data = data, aes(x = Name, y = (Mg_foliar))) +
   geom_boxplot()
 
 ### Al_foliar
-Al_foliar_lm = lm((Al_foliar) ~ Name, data = data)
+Al_foliar_lm = lm(as.formula(paste(dep_variables[14],
+                                   paste(ind_variables, collapse = "*"),
+                                   sep = "~")), data = data)
 #plot(resid(Al_foliar_lm) ~ fitted(Al_foliar_lm))
 anova(Al_foliar_lm)
-cld(emmeans(Al_foliar_lm, ~Name))
+cld(emmeans(Al_foliar_lm, ~elevation_fac * fire))
 
 ggplot(data = data, aes(x = Name, y = (Al_foliar))) +
   geom_boxplot()
 
 ### Zn_foliar
-Zn_foliar_lm = lm(log(Zn_foliar) ~ Name, data = data)
+Zn_foliar_lm = lm(as.formula(paste(dep_variables[15],
+                                   paste(ind_variables, collapse = "*"),
+                                   sep = "~")), data = data)
 #plot(resid(Zn_foliar_lm) ~ fitted(Zn_foliar_lm))
 anova(Zn_foliar_lm)
-cld(emmeans(Zn_foliar_lm, ~Name))
+cld(emmeans(Zn_foliar_lm, ~elevation_fac * fire))
 
 ggplot(data = data, aes(x = Name, y = (Zn_foliar))) +
   geom_boxplot()
 
 ### Ca_soil
-Ca_soil_lm = lm((Ca_soil) ~ Name, data = data)
+Ca_soil_lm = lm(as.formula(paste(dep_variables[16],
+                                 paste(ind_variables, collapse = "*"),
+                                 sep = "~")), data = data)
 #plot(resid(Ca_soil_lm) ~ fitted(Ca_soil_lm))
 anova(Ca_soil_lm)
-cld(emmeans(Ca_soil_lm, ~Name))
+cld(emmeans(Ca_soil_lm, ~elevation_fac * fire))
 
 ggplot(data = data, aes(x = Name, y = (Ca_soil))) +
   geom_boxplot()
 
 ### P_soil
-P_soil_lm = lm(log(P_soil) ~ Name, data = data)
+P_soil_lm = lm(as.formula(paste(dep_variables[17],
+                                paste(ind_variables, collapse = "*"),
+                                sep = "~")), data = data)
 #plot(resid(P_soil_lm) ~ fitted(P_soil_lm))
 anova(P_soil_lm)
-cld(emmeans(P_soil_lm, ~Name))
+cld(emmeans(P_soil_lm, ~elevation_fac * fire))
 
 ggplot(data = data, aes(x = Name, y = (P_soil))) +
   geom_boxplot()
 
 ### K_soil
-K_soil_lm = lm((K_soil) ~ Name, data = data)
+K_soil_lm = lm(as.formula(paste(dep_variables[18],
+                                paste(ind_variables, collapse = "*"),
+                                sep = "~")), data = data)
 #plot(resid(K_soil_lm) ~ fitted(K_soil_lm))
 anova(K_soil_lm)
-cld(emmeans(K_soil_lm, ~Name))
+cld(emmeans(K_soil_lm, ~elevation_fac * fire))
 
 ggplot(data = data, aes(x = Name, y = (K_soil))) +
   geom_boxplot()
 
 ### Mg_soil
-Mg_soil_lm = lm((Mg_soil) ~ Name, data = data)
+Mg_soil_lm = lm(as.formula(paste(dep_variables[19],
+                                 paste(ind_variables, collapse = "*"),
+                                 sep = "~")), data = data)
 #plot(resid(Mg_soil_lm) ~ fitted(Mg_soil_lm))
 anova(Mg_soil_lm)
-cld(emmeans(Mg_soil_lm, ~Name))
+cld(emmeans(Mg_soil_lm, ~elevation_fac * fire))
 
 ggplot(data = data, aes(x = Name, y = (Mg_soil))) +
   geom_boxplot()
 
 ### Al_soil
-Al_soil_lm = lm(log(Al_soil) ~ Name, data = data)
+Al_soil_lm = lm(as.formula(paste(dep_variables[20],
+                                 paste(ind_variables, collapse = "*"),
+                                 sep = "~")), data = data)
 #plot(resid(Al_soil_lm) ~ fitted(Al_soil_lm))
 anova(Al_soil_lm)
-cld(emmeans(Al_soil_lm, ~Name))
+cld(emmeans(Al_soil_lm, ~elevation_fac * fire))
 
 ggplot(data = data, aes(x = Name, y = (Al_soil))) +
   geom_boxplot()
 
 ### Zn_soil
-Zn_soil_lm = lm(log(Zn_soil) ~ Name, data = data)
+Zn_soil_lm = lm(as.formula(paste(dep_variables[21],
+                                 paste(ind_variables, collapse = "*"),
+                                 sep = "~")), data = data)
 #plot(resid(Zn_soil_lm) ~ fitted(Zn_soil_lm))
 anova(Zn_soil_lm)
-cld(emmeans(Zn_soil_lm, ~Name))
+cld(emmeans(Zn_soil_lm, ~elevation_fac * fire))
 
 ggplot(data = data, aes(x = Name, y = (Zn_soil))) +
   geom_boxplot()
 
 ### pH
-pH_lm = lm((pH) ~ Name, data = data)
+pH_lm = lm(as.formula(paste(dep_variables[22],
+                            paste(ind_variables, collapse = "*"),
+                            sep = "~")), data = data)
 #plot(resid(pH_lm) ~ fitted(pH_lm))
 anova(pH_lm)
-cld(emmeans(pH_lm, ~Name))
+cld(emmeans(pH_lm, ~elevation_fac * fire))
 
 ggplot(data = data, aes(x = Name, y = (pH))) +
   geom_boxplot()
 
 ### CEC
-CEC_lm = lm((CEC) ~ Name, data = data)
+CEC_lm = lm(as.formula(paste(dep_variables[23],
+                             paste(ind_variables, collapse = "*"),
+                             sep = "~")), data = data)
 #plot(resid(CEC_lm) ~ fitted(CEC_lm))
 anova(CEC_lm)
-cld(emmeans(CEC_lm, ~Name))
+cld(emmeans(CEC_lm, ~elevation_fac * fire))
 
 ggplot(data = data, aes(x = Name, y = (CEC))) +
   geom_boxplot()
 
 ### C_soil
-C_soil_lm = lm((C_soil) ~ Name, data = data)
+C_soil_lm = lm(as.formula(paste(dep_variables[24],
+                                paste(ind_variables, collapse = "*"),
+                                sep = "~")), data = data)
 #plot(resid(C_soil_lm) ~ fitted(C_soil_lm))
 anova(C_soil_lm)
-cld(emmeans(C_soil_lm, ~Name))
+cld(emmeans(C_soil_lm, ~elevation_fac * fire))
 
 ggplot(data = data, aes(x = Name, y = (C_soil))) +
   geom_boxplot()
 
 ### N_soil
-N_soil_lm = lm((N_soil) ~ Name, data = data)
+N_soil_lm = lm(as.formula(paste(dep_variables[25],
+                                paste(ind_variables, collapse = "*"),
+                                sep = "~")), data = data)
 #plot(resid(N_soil_lm) ~ fitted(N_soil_lm))
 anova(N_soil_lm)
-cld(emmeans(N_soil_lm, ~Name))
+cld(emmeans(N_soil_lm, ~elevation_fac * fire))
 
 ggplot(data = data, aes(x = Name, y = (N_soil))) +
   geom_boxplot()
 
 ### CN_soil
-CN_soil_lm = lm(log(CN_soil) ~ Name, data = data)
+CN_soil_lm = lm(as.formula(paste(dep_variables[26],
+                                 paste(ind_variables, collapse = "*"),
+                                 sep = "~")), data = data)
 #plot(resid(CN_soil_lm) ~ fitted(CN_soil_lm))
 anova(CN_soil_lm)
-cld(emmeans(CN_soil_lm, ~Name))
+cld(emmeans(CN_soil_lm, ~elevation_fac * fire))
 
 ggplot(data = data, aes(x = Name, y = (CN_soil))) +
   geom_boxplot()
 
 ### retention
-retention_lm = lm(asin(sqrt(0.01 * retention)) ~ Name, data = data)
+retention_lm = lm(as.formula(paste(dep_variables[27],
+                                   paste(ind_variables, collapse = "*"),
+                                   sep = "~")), data = data)
 #plot(resid(retention_lm) ~ fitted(retention_lm))
 anova(retention_lm)
-cld(emmeans(retention_lm, ~Name))
+cld(emmeans(retention_lm, ~elevation_fac * fire))
 
 ggplot(data = data, aes(x = Name, y = (retention))) +
   geom_boxplot()
