aa1c36e7c703b76b721e609bbc08be3668cc18f6
SmithEcophysLab <nick.gregory.smith@gmail.com>
2021-11-14 14:45:37 -0600

    mods based on jeffs last comments. to do: - fix neightbor distance data - last check of doc

--- data/clean_data.R
+++ data/clean_data.R
@@ -13,16 +13,20 @@ colnames(foliar) = c('ID', 'Name', 'Ca_foliar', 'P_foliar',
 # foliar$Mg_foliar <- foliar$Mg_foliar/1000
 # foliar$Al_foliar <- foliar$Al_foliar/1000
 # foliar$Zn_foliar <- foliar$Zn_foliar/1000
-biophysical = read.csv('raw/pitchpine_mdi_biophysical.csv', skip = 1)[, c(1:2, 4:6)]
+
+biophysical = read.csv('raw/pitchpine_mdi_biophysical_v2.csv', skip = 1)[, c(1:2, 4:6)]
 colnames(biophysical) = c('ID', 'Name', 'Height', 'Canopy', 'Diam')
-biophysical$Height <- biophysical$Height/100
-biophysical$Canopy <- biophysical$Canopy/100
-isotope = read.csv('raw/pitchpine_mdi_isotope.csv')[, 1:6]
+# biophysical$Height <- biophysical$Height/100
+# biophysical$Canopy <- biophysical$Canopy/100
+
+isotope = read.csv('raw/pitchpine_mdi_isotope_v2.csv')[, 1:6]
 colnames(isotope) = c('ID', 'Name', 'd13C', 'd15N', 'C_foliar', 'N_foliar')
 isotope[isotope$N_foliar > 15,]$N_foliar = NA
+
 retention = read.csv('raw/pitchpine_mdi_moistretent.csv', skip = 1)[, c(1, 2, 7)]
 colnames(retention) = c('ID', 'Name', 'Retention')
 retention$Retention = as.numeric(gsub("[\\%,]", "", retention$Retention))
+
 soil = read.csv('raw/pitchpine_mdi_soil.csv')[, 1:12]
 colnames(soil) = c('ID', 'Name', 'Ca_soil', 'P_soil', 'K_soil', 'Mg_soil', 'Al_soil', 'Zn_soil',
                    'pH', 'CEC', 'C_soil', 'N_soil')
@@ -32,7 +36,7 @@ colnames(soil) = c('ID', 'Name', 'Ca_soil', 'P_soil', 'K_soil', 'Mg_soil', 'Al_s
 # soil$Mg_soil <- soil$Mg_soil/1000
 # soil$Al_soil <- soil$Al_soil/1000
 # soil$Zn_soil <- soil$Zn_soil/1000
-geo = read.csv('raw/MDITreeGeoRasterData_2021.csv')
+geo = read.csv('raw/MDITreeGeoRasterData_2021_revised.csv')
 colnames(geo) = c('ID1', 'Name', 'Longitude', 'Latitude', 'Label', 'Elevation', 'Slope', 'Aspect')
 geo$ID = NA
 geo$ID[geo$Name == 'CAD'] = paste(geo$ID1[geo$Name == 'CAD'], '-', 'HIGHELEV-DIST', 
@@ -62,8 +66,14 @@ mdi_all$ID[mdi_all$Name == "CADCLIFFS"] = paste("CADCLIFFS", mdi_all$Rep[mdi_all
 mdi_all$ID[mdi_all$Name == "WOND"] = paste("WOND", mdi_all$Rep[mdi_all$Name == "WOND"], sep = "_")
 mdi_all$Rep <- NULL
 
+## remove samples 16-20
+mdi_all_clean <- mdi_all[1:60, ]
+mdi_all_clean[, 1]
 
-write.csv(mdi_all, 'mdi_all_clean.csv', row.names = F)
+## add in year of sampling
+mdi_all_clean$year <- c(rep(c(rep(2019, 10), rep(2020, 5)), 4))
+
+write.csv(mdi_all_clean, 'mdi_all_clean.csv', row.names = F)
 
 
 
